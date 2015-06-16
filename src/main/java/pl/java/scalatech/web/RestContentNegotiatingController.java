cd8e3d6be024767873a9372192cdf7b723757d95
Slawomir Borowiec <przodownik@tlen.pl>
2015-06-16 11:52:13 +0200

    rest mockito + matchers

--- src/main/java/pl/java/scalatech/web/RestContentNegotiatingController.java
+++ src/main/java/pl/java/scalatech/web/RestContentNegotiatingController.java
@@ -2,22 +2,37 @@ package pl.java.scalatech.web;
 
 import java.math.BigDecimal;
 
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import pl.java.scalatech.entity.BankAccount;
+import pl.java.scalatech.repository.BankAccountRepository;
 
 @RestController
 @RequestMapping(value = RestContentNegotiatingController.URL)
+@RequiredArgsConstructor(onConstructor = @_(@Autowired))
 public class RestContentNegotiatingController {
 
     public final static String URL = "/cn";
+
+    private final @NonNull BankAccountRepository bankAccountRepository;
 
     @RequestMapping
     @ResponseBody
     public BankAccount ba() {
         return BankAccount.builder().amount(new BigDecimal("1000")).creditCardNumer("120003430023").debit(new BigDecimal("900")).build();
+    }
+
+    @RequestMapping("/{id}")
+    @ResponseBody
+    public BankAccount baById(@PathVariable("id") Long id) {
+        return bankAccountRepository.findOne(id);
     }
 
 }
--- src/test/java/pl/java/scalatech/rest/RestDCTest.java
+++ src/test/java/pl/java/scalatech/rest/RestDCTest.java
@@ -1,14 +1,20 @@
 package pl.java.scalatech.rest;
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.math.BigDecimal;
+
 import org.crsh.console.jline.internal.Log;
+import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
@@ -20,6 +26,8 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import pl.java.scalatech.RestStubDcApplication;
+import pl.java.scalatech.entity.BankAccount;
+import pl.java.scalatech.repository.BankAccountRepository;
 import pl.java.scalatech.web.HealthController;
 import pl.java.scalatech.web.RestContentNegotiatingController;
 
@@ -31,6 +39,9 @@ public class RestDCTest {
     private WebApplicationContext webApplicationContext;
 
     private MockMvc mockMvc;
+
+    @Mock
+    private BankAccountRepository bankAccountRepository;
 
     @Before
     public void setup() {
@@ -44,14 +55,36 @@ public class RestDCTest {
 
     @Test
     public void shouldHealthControllerResponse() throws Exception {
-        this.mockMvc.perform(get(HealthController.URL)).andExpect(status().isOk());
+        this.mockMvc.perform(get(HealthController.URL)).andExpect(status().isOk()).andExpect(content().string(Matchers.not(Matchers.isEmptyOrNullString())));
     }
 
     @Test
-    public void shouldRestContextNegotiationWork() throws Exception {
+    public void shouldRestContextNegotiationJsonWork() throws Exception {
         this.mockMvc.perform(get(RestContentNegotiatingController.URL).accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith("application/json")).andExpect(jsonPath("$.creditCardNumer").value("120003430023"))
-                .andExpect(jsonPath("$.debit").value(900)).andExpect(jsonPath("$.amount").value(1000));
+                .andExpect(jsonPath("$.debit").value(900)).andExpect(jsonPath("$.amount", is(1000)));
+        String response = this.mockMvc.perform(get(RestContentNegotiatingController.URL)).andReturn().getResponse().getContentAsString();
+        Log.info("+++ cn  response {}", response);
+
+    }
+
+    @Test
+    public void shouldRestContextNegotiationJsonWithParamsWork() throws Exception {
+        Mockito.when(bankAccountRepository.findOne(1l)).thenReturn(
+                BankAccount.builder().amount(new BigDecimal("1000")).creditCardNumer("120003430023").debit(new BigDecimal("900")).build());
+
+        this.mockMvc.perform(get(RestContentNegotiatingController.URL).accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith("application/json")).andExpect(jsonPath("$.creditCardNumer").value("120003430023"))
+                .andExpect(jsonPath("$.debit").value(900)).andExpect(jsonPath("$.amount", is(1000)));
+        String response = this.mockMvc.perform(get(RestContentNegotiatingController.URL)).andReturn().getResponse().getContentAsString();
+        Log.info("+++ cn  response {}", response);
+
+    }
+
+    @Test
+    public void shouldRestContextNegotiationXmlWork() throws Exception {
+        this.mockMvc.perform(get(RestContentNegotiatingController.URL).accept(MediaType.APPLICATION_XML_VALUE)).andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith("application/xml"));
         String response = this.mockMvc.perform(get(RestContentNegotiatingController.URL)).andReturn().getResponse().getContentAsString();
         Log.info("+++ cn  response {}", response);
 
